67fb7630aea2958147ad2dac2bf7e8f6b1f6e216
Erik Erlandson <eerlands@redhat.com>
2019-07-02 10:15:12 -0700

    add sum field to verify aggregation logic

--- sql/core/src/main/scala/org/apache/spark/sql/execution/aggregate/CountSerDe.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/aggregate/CountSerDe.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.countSerDe
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
@@ -28,7 +29,7 @@ import org.apache.spark.sql.types._
 
 
 @SQLUserDefinedType(udt = classOf[CountSerDeUDT])
-case class CountSerDeSQL(nSer: Int, nDeSer: Int)
+case class CountSerDeSQL(nSer: Int, nDeSer: Int, sum: Double)
 
 class CountSerDeUDT extends UserDefinedType[CountSerDeSQL] {
   def userClass: Class[CountSerDeSQL] = classOf[CountSerDeSQL]
@@ -40,18 +41,20 @@ class CountSerDeUDT extends UserDefinedType[CountSerDeSQL] {
   def sqlType: DataType = StructType(
     StructField("nSer", IntegerType, false) ::
     StructField("nDeSer", IntegerType, false) ::
+    StructField("sum", DoubleType, false) ::
     Nil)
 
   def serialize(sql: CountSerDeSQL): Any = {
-    val row = new GenericInternalRow(2)
+    val row = new GenericInternalRow(3)
     row.setInt(0, 1 + sql.nSer)
     row.setInt(1, sql.nDeSer)
+    row.setDouble(2, sql.sum)
     row
   }
 
   def deserialize(any: Any): CountSerDeSQL = any match {
-    case row: InternalRow if (row.numFields == 2) =>
-      CountSerDeSQL(row.getInt(0), 1 + row.getInt(1))
+    case row: InternalRow if (row.numFields == 3) =>
+      CountSerDeSQL(row.getInt(0), 1 + row.getInt(1), row.getDouble(2))
     case u => throw new Exception(s"failed to deserialize: $u")
   }
 
@@ -67,14 +70,18 @@ class CountSerDeUDT extends UserDefinedType[CountSerDeSQL] {
 
 case object CountSerDeUDT extends CountSerDeUDT
 
-case object CountSerDeUDIA extends UserDefinedImperativeAggregator[CountSerDeSQL] {
+case object CountSerDeUDIA extends UserDefinedImperativeAggregator[CountSerDeSQL] with Logging {
   def inputSchema: StructType = StructType(StructField("x", DoubleType) :: Nil)
   def resultType: DataType = CountSerDeUDT
   def deterministic: Boolean = true
-  def empty(): CountSerDeSQL = CountSerDeSQL(0, 0)
-  def update(agg: CountSerDeSQL, input: Row): CountSerDeSQL = agg
+  def empty(): CountSerDeSQL = CountSerDeSQL(0, 0, 0)
+  def update(agg: CountSerDeSQL, input: Row): CountSerDeSQL = {
+    val u = agg.copy(sum = agg.sum + input.getDouble(0))
+    //log.warn(s"agg= $agg  input= $input  u= $u")
+    u
+  }
   def merge(agg1: CountSerDeSQL, agg2: CountSerDeSQL): CountSerDeSQL =
-    CountSerDeSQL(agg1.nSer + agg2.nSer, agg1.nDeSer + agg2.nDeSer)
+    CountSerDeSQL(agg1.nSer + agg2.nSer, agg1.nDeSer + agg2.nDeSer, agg1.sum + agg2.sum)
   def evaluate(agg: CountSerDeSQL): Any = agg
 
   import java.io._
@@ -91,7 +98,7 @@ case object CountSerDeUDIA extends UserDefinedImperativeAggregator[CountSerDeSQL
   def serialize(agg: CountSerDeSQL): Array[Byte] = {
     val bufout = new ByteArrayOutputStream()
     val obout = new ObjectOutputStream(bufout)
-    obout.writeObject(CountSerDeSQL(agg.nSer + 1, agg.nDeSer))
+    obout.writeObject(agg.copy(nSer = agg.nSer + 1))
     bufout.toByteArray
   }
   def deserialize(data: Array[Byte]): CountSerDeSQL = {
@@ -99,7 +106,7 @@ case object CountSerDeUDIA extends UserDefinedImperativeAggregator[CountSerDeSQL
     val obin = new ObjectInputStreamWithCustomClassLoader(bufin)
 
     val agg = obin.readObject().asInstanceOf[CountSerDeSQL]
-    CountSerDeSQL(agg.nSer, agg.nDeSer + 1)
+    agg.copy(nDeSer = agg.nDeSer + 1)
   }
 }
 
@@ -113,18 +120,18 @@ case object CountSerDeUDAF extends UserDefinedAggregateFunction {
   def dataType: DataType = CountSerDeUDT
 
   def initialize(buf: MutableAggregationBuffer): Unit = {
-    buf(0) = CountSerDeSQL(0, 0)
+    buf(0) = CountSerDeSQL(0, 0, 0)
   }
 
   def update(buf: MutableAggregationBuffer, input: Row): Unit = {
     val sql = buf.getAs[CountSerDeSQL](0)
-    buf(0) = sql
+    buf(0) = sql.copy(sum = sql.sum + input.getDouble(0))
   }
 
   def merge(buf1: MutableAggregationBuffer, buf2: Row): Unit = {
     val sql1 = buf1.getAs[CountSerDeSQL](0)
     val sql2 = buf2.getAs[CountSerDeSQL](0)
-    buf1(0) = CountSerDeSQL(sql1.nSer + sql2.nSer, sql1.nDeSer + sql2.nDeSer)
+    buf1(0) = CountSerDeSQL(sql1.nSer + sql2.nSer, sql1.nDeSer + sql2.nDeSer, sql1.sum + sql2.sum)
   }
 
   def evaluate(buf: Row): Any = buf.getAs[CountSerDeSQL](0)
