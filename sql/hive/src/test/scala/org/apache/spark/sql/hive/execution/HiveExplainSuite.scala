264a2599d45e6a7549e9457e6a4e7c7352fe5794
Wenchen Fan <wenchen@databricks.com>
2019-08-07 11:44:01 +0800

    address comment

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveExplainSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveExplainSuite.scala
@@ -208,12 +208,21 @@ class HiveExplainSuite extends QueryTest with SQLTestUtils with TestHiveSingleto
   }
 
   test("SPARK-28595: explain should not trigger partition listing") {
-    HiveCatalogMetrics.reset()
-    withTable("t") {
-      sql("CREATE TABLE t USING json PARTITIONED BY (j) AS SELECT 1 i, 2 j")
-      assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount == 0)
-      spark.table("t").explain()
-      assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount == 0)
+    Seq(true, false).foreach { legacyBucketedScan =>
+      withSQLConf(
+        SQLConf.LEGACY_BUCKETED_TABLE_SCAN_OUTPUT_ORDERING.key -> legacyBucketedScan.toString) {
+        HiveCatalogMetrics.reset()
+        withTable("t") {
+          sql("CREATE TABLE t USING json PARTITIONED BY (j) AS SELECT 1 i, 2 j")
+          assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount == 0)
+          spark.table("t").explain()
+          if (legacyBucketedScan) {
+            assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount > 0)
+          } else {
+            assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount == 0)
+          }
+        }
+      }
     }
   }
 }
