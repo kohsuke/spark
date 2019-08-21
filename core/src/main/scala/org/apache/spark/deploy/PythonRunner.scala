2a34f6f613a1c8337902a84e776ee5f26663dc62
Ryan Blue <blue@apache.org>
2019-08-21 15:30:16 -0700

    SPARK-28843: Set OMP_NUM_THREADS to executor cores for python.

--- core/src/main/scala/org/apache/spark/deploy/PythonRunner.scala
+++ core/src/main/scala/org/apache/spark/deploy/PythonRunner.scala
@@ -91,6 +91,7 @@ object PythonRunner {
     // python process is through environment variable.
     sparkConf.get(PYSPARK_PYTHON).foreach(env.put("PYSPARK_PYTHON", _))
     sys.env.get("PYTHONHASHSEED").foreach(env.put("PYTHONHASHSEED", _))
+    sparkConf.getOption("spark.executor.cores").map(env.put("OMP_NUM_THREADS", _))
     builder.redirectErrorStream(true) // Ugly but needed for stdout and stderr to synchronize
     try {
       val process = builder.start()
