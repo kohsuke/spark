e0f62000cb45c03f48882c06c7ce875a91f839fc
HyukjinKwon <gurwls223@apache.org>
2019-11-20 19:12:50 +0900

    Implement Dataset.tail API

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/commands.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/commands.scala
@@ -82,6 +82,14 @@ case class ExecutedCommandExec(cmd: RunnableCommand) extends LeafExecNode {
 
   override def executeTake(limit: Int): Array[InternalRow] = sideEffectResult.take(limit).toArray
 
+  override def executeTail(limit: Int): Array[InternalRow] = {
+    if (limit <= 0) return Array.empty[InternalRow]
+    val slidingIter = sideEffectResult.sliding(limit)
+    var taken: Seq[InternalRow] = Seq.empty[InternalRow]
+    while(slidingIter.hasNext) { taken = slidingIter.next() }
+    taken.toArray
+  }
+
   protected override def doExecute(): RDD[InternalRow] = {
     sqlContext.sparkContext.parallelize(sideEffectResult, 1)
   }
@@ -118,6 +126,14 @@ case class DataWritingCommandExec(cmd: DataWritingCommand, child: SparkPlan)
   override def executeToIterator: Iterator[InternalRow] = sideEffectResult.toIterator
 
   override def executeTake(limit: Int): Array[InternalRow] = sideEffectResult.take(limit).toArray
+
+  override def executeTail(limit: Int): Array[InternalRow] = {
+    if (limit <= 0) return Array.empty[InternalRow]
+    val slidingIter = sideEffectResult.sliding(limit)
+    var taken: Seq[InternalRow] = Seq.empty[InternalRow]
+    while(slidingIter.hasNext) { taken = slidingIter.next() }
+    taken.toArray
+  }
 
   protected override def doExecute(): RDD[InternalRow] = {
     sqlContext.sparkContext.parallelize(sideEffectResult, 1)
