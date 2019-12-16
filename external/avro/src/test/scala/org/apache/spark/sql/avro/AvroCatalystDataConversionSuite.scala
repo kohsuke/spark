19a821af109bcabe56c324919dff4796936d3e56
Steven Aerts <steven.aerts@gmail.com>
2019-12-16 14:13:48 +0100

    [SPARK-30267][SQL] Avro arrays can be of any List
    The Deserializer assumed that avro arrays are always of type
    GenericData$Array which is not the case.
    Assuming they are from java.util.List is safer and fixes a
    ClassCastException.

--- external/avro/src/test/scala/org/apache/spark/sql/avro/AvroCatalystDataConversionSuite.scala
+++ external/avro/src/test/scala/org/apache/spark/sql/avro/AvroCatalystDataConversionSuite.scala
@@ -127,6 +127,25 @@ class AvroCatalystDataConversionSuite extends SparkFunSuite
     }
   }
 
+  test(s"array of nested schema with seed") {
+    val seed = scala.util.Random.nextLong()
+    val rand = new scala.util.Random(seed)
+    val schema = StructType(
+      StructField("a",
+        ArrayType(
+          RandomDataGenerator.randomNestedSchema(rand, 10, testingTypes)
+          , containsNull = false)
+        , nullable = false) :: Nil
+    )
+
+    withClue(s"Schema: $schema\nseed: $seed") {
+      val data = RandomDataGenerator.randomRow(rand, schema)
+      val converter = CatalystTypeConverters.createToCatalystConverter(schema)
+      val input = Literal.create(converter(data), schema)
+      roundTripTest(input)
+    }
+  }
+
   test("read int as string") {
     val data = Literal(1)
     val avroTypeJson =
