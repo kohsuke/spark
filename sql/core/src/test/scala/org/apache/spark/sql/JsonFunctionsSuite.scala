780c47bc57b8f5f21440080d42d20c354301fa3e
iRakson <raksonrakesh@gmail.com>
2020-04-04 19:51:43 +0530

    review comment fix

--- sql/core/src/test/scala/org/apache/spark/sql/JsonFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/JsonFunctionsSuite.scala
@@ -653,4 +653,12 @@ class JsonFunctionsSuite extends QueryTest with SharedSparkSession {
       assert(json_tuple_result === len)
     }
   }
+
+  test("json_array_length") {
+    val df = Seq(1).toDF("json")
+    val errMsg = intercept[AnalysisException] {
+      df.selectExpr("json_array_length(json)")
+    }.getMessage
+    assert(errMsg.contains("due to data type mismatch"))
+  }
 }
