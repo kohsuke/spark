b53e848b283d22958c0158af0b237b62c6cafcf6
Prakhar Jain <prakharjain09@gmail.com>
2020-05-04 19:26:54 +0530

    comment all tests added to confirm if newly written tests are messing up things in some way

--- core/src/test/scala/org/apache/spark/storage/BlockManagerDecommissionSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockManagerDecommissionSuite.scala
@@ -38,68 +38,68 @@ class BlockManagerDecommissionSuite extends SparkFunSuite with LocalSparkContext
     sc = new SparkContext("local-cluster[2, 1, 1024]", "test", conf)
   }
 
-  test(s"verify that an already running task which is going to cache data succeeds " +
-    s"on a decommissioned executor") {
-    // Create input RDD with 10 partitions
-    val input = sc.parallelize(1 to 10, 10)
-    val accum = sc.longAccumulator("mapperRunAccumulator")
-    // Do a count to wait for the executors to be registered.
-    input.count()
-
-    // Create a new RDD where we have sleep in each partition, we are also increasing
-    // the value of accumulator in each partition
-    val sleepyRdd = input.mapPartitions { x =>
-      Thread.sleep(500)
-      accum.add(1)
-      x
-    }
-
-    // Listen for the job
-    val sem = new Semaphore(0)
-    val taskEndEvents = ArrayBuffer.empty[SparkListenerTaskEnd]
-    sc.addSparkListener(new SparkListener {
-      override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
-       sem.release()
-      }
-
-      override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
-        taskEndEvents.append(taskEnd)
-      }
-    })
-
-    // Cache the RDD lazily
-    sleepyRdd.persist()
-
-    // Start the computation of RDD - this step will also cache the RDD
-    val asyncCount = sleepyRdd.countAsync()
-
-    // Wait for the job to have started
-    sem.acquire(1)
-
-    // Give Spark a tiny bit to start the tasks after the listener says hello
-    Thread.sleep(100)
-    // Decommission one of the executor
-    val sched = sc.schedulerBackend.asInstanceOf[StandaloneSchedulerBackend]
-    val execs = sched.getExecutorIds()
-    assert(execs.size == 2, s"Expected 2 executors but found ${execs.size}")
-    val execToDecommission = execs.head
-    sched.decommissionExecutor(execToDecommission)
-
-    // Wait for job to finish
-    val asyncCountResult = ThreadUtils.awaitResult(asyncCount, 6.seconds)
-    assert(asyncCountResult === 10)
-    // All 10 tasks finished, so accum should have been increased 10 times
-    assert(accum.value === 10)
-
-    // All tasks should be successful, nothing should have failed
-    sc.listenerBus.waitUntilEmpty()
-    assert(taskEndEvents.size === 10) // 10 mappers
-    assert(taskEndEvents.map(_.reason).toSet === Set(Success))
-
-    // Since the RDD is cached, so further usage of same RDD should use the
-    // cached data. Original RDD partitions should not be recomputed i.e. accum
-    // should have same value like before
-    assert(sleepyRdd.count() === 10)
-    assert(accum.value === 10)
-  }
+//  test(s"verify that an already running task which is going to cache data succeeds " +
+//    s"on a decommissioned executor") {
+//    // Create input RDD with 10 partitions
+//    val input = sc.parallelize(1 to 10, 10)
+//    val accum = sc.longAccumulator("mapperRunAccumulator")
+//    // Do a count to wait for the executors to be registered.
+//    input.count()
+//
+//    // Create a new RDD where we have sleep in each partition, we are also increasing
+//    // the value of accumulator in each partition
+//    val sleepyRdd = input.mapPartitions { x =>
+//      Thread.sleep(500)
+//      accum.add(1)
+//      x
+//    }
+//
+//    // Listen for the job
+//    val sem = new Semaphore(0)
+//    val taskEndEvents = ArrayBuffer.empty[SparkListenerTaskEnd]
+//    sc.addSparkListener(new SparkListener {
+//      override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
+//       sem.release()
+//      }
+//
+//      override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
+//        taskEndEvents.append(taskEnd)
+//      }
+//    })
+//
+//    // Cache the RDD lazily
+//    sleepyRdd.persist()
+//
+//    // Start the computation of RDD - this step will also cache the RDD
+//    val asyncCount = sleepyRdd.countAsync()
+//
+//    // Wait for the job to have started
+//    sem.acquire(1)
+//
+//    // Give Spark a tiny bit to start the tasks after the listener says hello
+//    Thread.sleep(100)
+//    // Decommission one of the executor
+//    val sched = sc.schedulerBackend.asInstanceOf[StandaloneSchedulerBackend]
+//    val execs = sched.getExecutorIds()
+//    assert(execs.size == 2, s"Expected 2 executors but found ${execs.size}")
+//    val execToDecommission = execs.head
+//    sched.decommissionExecutor(execToDecommission)
+//
+//    // Wait for job to finish
+//    val asyncCountResult = ThreadUtils.awaitResult(asyncCount, 6.seconds)
+//    assert(asyncCountResult === 10)
+//    // All 10 tasks finished, so accum should have been increased 10 times
+//    assert(accum.value === 10)
+//
+//    // All tasks should be successful, nothing should have failed
+//    sc.listenerBus.waitUntilEmpty()
+//    assert(taskEndEvents.size === 10) // 10 mappers
+//    assert(taskEndEvents.map(_.reason).toSet === Set(Success))
+//
+//    // Since the RDD is cached, so further usage of same RDD should use the
+//    // cached data. Original RDD partitions should not be recomputed i.e. accum
+//    // should have same value like before
+//    assert(sleepyRdd.count() === 10)
+//    assert(accum.value === 10)
+//  }
 }
