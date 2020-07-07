620fb5d226f7fbc551f0f2eaca9ad3c4c239c1d0
Devesh Agrawal <devesh.agrawal@gmail.com>
2020-07-07 15:22:49 -0700

    Plumb whether a worker would also be decommissioned along with executor
    This PR is a giant plumbing PR that plumbs an `ExecutorDecommissionInfo` along
    with the DecommissionExecutor message.
    
    The primary motivation is to know whether a decommissioned executor
    would also be loosing shuffle files -- and thus it is important to know
    whether the host would also be decommissioned. This is similar to the
    existing `workerLost` field in the `ExecutorProcessLost` message.
    
    In the future, this `ExecutorDecommissionInfo` can be embellished for
    knowing how long the executor has to live for scenarios like Cloud spot
    kills (or Yarn preemption) and the like.

--- core/src/main/scala/org/apache/spark/deploy/master/Master.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/Master.scala
@@ -908,7 +908,7 @@ private[deploy] class Master(
         logInfo("Telling app of decommission executors")
         exec.application.driver.send(ExecutorUpdated(
           exec.id, ExecutorState.DECOMMISSIONED,
-          Some("worker decommissioned"), None, workerLost = false))
+          Some("worker decommissioned"), None, workerLost = true))
         exec.state = ExecutorState.DECOMMISSIONED
         exec.application.removeExecutor(exec)
       }
