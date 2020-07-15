11d138bf188212f7248592c236dd1831e0b8affa
Cheng Su <chengsu@fb.com>
2020-07-15 09:47:06 -0700

    Set smaller default config value for SHJ

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2676,7 +2676,7 @@ object SQLConf {
       .version("3.1.0")
       .intConf
       .checkValue(_ > 0, "The difference must be positive.")
-      .createWithDefault(4)
+      .createWithDefault(2)
 
   /**
    * Holds information about keys that have been deprecated.
