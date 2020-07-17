ccc1844788b2c7381499622469efd1ff1b126c9c
xuewei.linxuewei <xuewei.linxuewei@alibaba-inc.com>
2020-07-17 17:17:22 +0800

    [SPARK-32290][SQL] NotInSubquery SingleColumn Optimize
    sub commit
    1. revert code in BroadcastHashJoinExec.
    2. revert code in BroadcastNestedLoopJoinExec.
    3. move code into new ExecNode call BroadcastNullAwareHashJoinExec, currently this ExecNode only handle SingleColumn NotInSubquery case, it might be extended into supporting multi column.
    4. add NotInSubqueryHashJoinTestSuite with NOT_IN_SUBQUERY_HASH_JOIN_ENABLED = true to run E2E SQL Coverage cases for all subquery case.
    5. resolve other commiter's comment.
    
    Test.
    * NotInSubqueryHashJoinTestSuite passed.
    * SQLQueryTestSuite passed.
    * Local E2E with NOT_IN_SUBQUERY_HASH_JOIN_ENABLED = true, passed and verified BroadcastNullAwareHashJoinExec applied.
    * Local E2E with NOT_IN_SUBQUERY_HASH_JOIN_ENABLED = true, spark.sql.adaptive.enabled = true, passed and verified BroadcastNullAwareHashJoinExec applied.
    * TPCH Query 16.
    
    Change-Id: I29c22d3db6d0684595ca494fd33f768b4d9a2297

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2665,22 +2665,16 @@ object SQLConf {
       .checkValue(_ > 0, "The difference must be positive.")
       .createWithDefault(4)
 
-  val NOT_IN_SUBQUERY_SINGLE_COLUMN_OPTIMIZE_ENABLED =
-    buildConf("spark.sql.notInSubquery.singleColumn.optimize.enabled")
+  val NOT_IN_SUBQUERY_HASH_JOIN_ENABLED =
+    buildConf("spark.sql.notInSubquery.hashJoin.enabled")
       .internal()
-      .doc("When true, single column not in subquery execution in BroadcastNestedLoopJoinExec " +
-        "will be optimized from M*N calculation into M*log(N) calculation using HashMap lookup " +
-        "instead of Looping lookup.")
+      .doc("When true, not in subquery execution will be planed into " +
+        "BroadcastNullAwareHashJoinExec, " +
+        "optimized from O(M*N) calculation into O(M) calculation " +
+        "using Hash lookup instead of Looping lookup." +
+        "Only support for singleColumn not in subquery for now.")
       .booleanConf
       .createWithDefault(false)
-
-  val NOT_IN_SUBQUERY_SINGLE_COLUMN_OPTIMIZE_ROW_COUNT_THRESHOLD =
-    buildConf("spark.sql.notInSubquery.singleColumn.optimize.rowCountThreshold")
-      .internal()
-      .doc("Build side rowCount in BroadcastNestedLoopJoinExec must be less than this value " +
-        "before spark.sql.notInSubquery.singleColumn.optimize actually works.")
-      .intConf
-      .createWithDefault(10000)
 
   /**
    * Holds information about keys that have been deprecated.
@@ -3277,12 +3271,8 @@ class SQLConf extends Serializable with Logging {
   def legacyAllowCastNumericToTimestamp: Boolean =
     getConf(SQLConf.LEGACY_ALLOW_CAST_NUMERIC_TO_TIMESTAMP)
 
-  def notInSubquerySingleColumnOptimizeEnabled: Boolean =
-    getConf(SQLConf.NOT_IN_SUBQUERY_SINGLE_COLUMN_OPTIMIZE_ENABLED)
-
-  def notInSubquerySingleColumnOptimizeRowCountThreshold: Int = {
-    getConf(SQLConf.NOT_IN_SUBQUERY_SINGLE_COLUMN_OPTIMIZE_ROW_COUNT_THRESHOLD)
-  }
+  def notInSubqueryHashJoinEnabled: Boolean =
+    getConf(SQLConf.NOT_IN_SUBQUERY_HASH_JOIN_ENABLED)
 
   /** ********************** SQLConf functionality methods ************ */
 
