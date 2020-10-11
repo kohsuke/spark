7081049015dcaa269982b37ee69b109babc9d56d
yi.wu <yi.wu@databricks.com>
2020-10-11 14:18:24 +0800

    update test

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/DecommissionSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/DecommissionSuite.scala
@@ -41,8 +41,7 @@ private[spark] trait DecommissionSuite { k8sSuite: KubernetesSuite =>
       mainClass = "",
       expectedLogOnCompletion = Seq(
         "Finished waiting, stopping Spark",
-        "Asking BlockManagers", "to decommissioning",
-        "Asking executor", "to decommissioning.",
+        "Decommission executors",
         "Final accumulator value is: 100"),
       appArgs = Array.empty[String],
       driverPodChecker = doBasicDriverPyPodCheck,
