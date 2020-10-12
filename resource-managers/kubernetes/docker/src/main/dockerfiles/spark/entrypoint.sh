0d15ce30dfcb06989acc63de70b77b5daf737379
Prashant Sharma <prashsh1@in.ibm.com>
2020-10-12 11:30:13 +0530

    Update entrypoint.sh

--- resource-managers/kubernetes/docker/src/main/dockerfiles/spark/entrypoint.sh
+++ resource-managers/kubernetes/docker/src/main/dockerfiles/spark/entrypoint.sh
@@ -63,7 +63,7 @@ fi
 
 if ! [ -z ${SPARK_CONF_DIR+x} ]; then
   SPARK_CLASSPATH="$SPARK_CONF_DIR:$SPARK_CLASSPATH";
-elif ! [ -z ${SPARK_HOME+x}]; then
+elif ! [ -z ${SPARK_HOME+x} ]; then
   SPARK_CLASSPATH="$SPARK_HOME/conf:$SPARK_CLASSPATH";
 fi
 
