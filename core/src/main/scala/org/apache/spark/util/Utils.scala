c711c8669d4ea09b6a18147b82f5951ac9c24b36
Chandni Singh <singh.chandni@gmail.com>
2020-11-10 11:17:56 -0800

    Renamed isPushBasedShuffleEnabled to isPushShuffleEnabled and fixed the comment

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -2543,10 +2543,8 @@ private[spark] object Utils extends Logging {
 
   /**
    * Push based shuffle can only be enabled when external shuffle service is enabled.
-   * In the initial version, we cannot support pushed based shuffle and adaptive execution
-   * at the same time. Will improve this in a later version.
    */
-  def isPushBasedShuffleEnabled(conf: SparkConf): Boolean = {
+  def isPushShuffleEnabled(conf: SparkConf): Boolean = {
     conf.get(PUSH_SHUFFLE_ENABLED) && conf.get(SHUFFLE_SERVICE_ENABLED)
   }
 
